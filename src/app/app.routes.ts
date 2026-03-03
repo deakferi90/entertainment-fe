f3ce6b905dafec6e50c1b4476408ec3e3bc3f117
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2026-03-03 16:04:49 +0200

    started implementing login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
+  },
+];
---
+++ src/app/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  movieImg: string = 'assets/icon-movie.png';
+}
