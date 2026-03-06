92459870bfcae7b0773b6085e360cc4c31410cf2
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2026-03-06 16:56:31 +0200

    started working on the UI of the login page

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
 export class Login {
   movieImg: string = 'assets/icon-movie.png';
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 }
