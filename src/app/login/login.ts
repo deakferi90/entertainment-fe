440ed557820f2c350d4a23189dac789452d42736
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2026-03-09 11:04:45 +0200

    worked on the UI of the login and signup page

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -19,9 +19,20 @@ import { CommonModule } from '@angular/common';
 })
 export class Login {
   movieImg: string = 'assets/icon-movie.png';
+  private router = inject(Router);
 
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/),
+  ]);
+
+  redirectoSignUp() {
+    this.router.navigate(['signup']);
+  }
 }
--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
 export class Signup {
+  private router = inject(Router);
+  movieImg: string = 'assets/icon-movie.png';
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/),
+  ]);
+
+  redirectoLogin() {
+    this.router.navigate(['login']);
+  }
 }
