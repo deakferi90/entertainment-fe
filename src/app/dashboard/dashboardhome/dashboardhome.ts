48b43eb6fb988f0ffa74ebab4123825645cfc948
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2026-03-23 14:55:10 +0200

    been working on bookmark logic

--- src/app/dashboard/dashboardhome/dashboardhome.ts
+++ src/app/dashboard/dashboardhome/dashboardhome.ts
@@ -2,20 +2,29 @@ import { Component, computed, signal } from '@angular/core';
 import { MovieInterface } from '../movies/movie.interface';
 import { Dashboardservice } from './dashboardservice';
 import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboardhome',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboardhome.html',
   styleUrl: './dashboardhome.scss',
 })
 export class Dashboardhome {
   allContent = signal<MovieInterface[]>([]);
+  bookmark: string = 'assets/bookmark2.svg';
   filterText = signal('');
+  bookmarked = signal(false);
+  selectedIndex: number | null = null;
+  bookmarkedItems = signal<boolean[]>([]);
   constructor(private movieService: Dashboardservice) {}
 
   ngOnInit() {
     this.displayAll();
+  }
+
+  onSelect(index: number) {
+    this.selectedIndex = index;
   }
 
   displayAll() {
@@ -23,9 +32,10 @@ export class Dashboardhome {
       movies: this.movieService.getAllMovies(),
       series: this.movieService.getTvSeries(),
     }).subscribe(({ movies, series }) => {
-      this.allContent.set(
-        [...movies, ...series].sort((a, b) => a.title.localeCompare(b.title)),
-      );
+      const data = [...movies, ...series];
+      this.allContent.set(data);
+
+      this.bookmarkedItems.set(new Array(data.length).fill(false));
     });
   }
 
@@ -39,5 +49,11 @@ export class Dashboardhome {
   filterSearch(event: Event) {
     const val = (event.target as HTMLInputElement).value;
     this.filterText.set(val);
+  }
+
+  toggleBookmark(index: number) {
+    const current = this.bookmarkedItems();
+    current[index] = !current[index];
+    this.bookmarkedItems.set([...current]);
   }
 }
